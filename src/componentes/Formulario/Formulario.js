ab87fc6691ddfe6d35b930606cdefaa72d653bf6
Evandro Kaibara <yujievandro@gmail.com>
2024-02-21 20:18:20 -0300

    Iniciando Backend - Campos de Texto

--- src/componentes/Formulario/Formulario.js
+++ src/componentes/Formulario/Formulario.js
@@ -1,22 +1,51 @@
+import { useState } from 'react'
 import Botao from '../Botao/Botao'
 import CampoTexto from '../CampoTexto/CampoTexto'
 import ListaSuspensa from '../ListaSuspensa/ListaSuspensa'
 import './Formulario.css'
 
-const generos = [
-    'Masculino',
-    'Feminino'
-]
-
 function Formulario(){
+    const generos = [
+        'Masculino',
+        'Feminino'
+    ]
+
+    const [nome, setNome] = useState('')
+    const [idade, setIdade] = useState('')
+    const [imagem, setImagem] = useState('')
+    const [genero, setGenero] = useState('')
+
     return(
         <section className='formulario'>
             <form>
                 <h2>Preencha os dados para criar o card do animal</h2>
-                <CampoTexto label="Nome" placeholder="Digite o nome" />
-                <CampoTexto label="Idade" placeholder="Digite a idade" />
-                <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
-                <ListaSuspensa label="Gênero" itens={generos}/>
+                <CampoTexto 
+                    label="Nome" 
+                    placeholder="Digite o nome"
+                    obrigatorio={true}
+                    valor={nome}
+                    aoAlterado={valor => setNome(valor)} 
+                />
+                <CampoTexto 
+                    label="Idade" 
+                    placeholder="Digite a idade"
+                    obrigatorio={true}
+                    valor={idade}
+                    aoAlterado={valor => setIdade(valor)}
+                />
+                <CampoTexto 
+                    label="Imagem"
+                    placeholder="Digite o endereço da imagem"
+                    valor={imagem}
+                    aoAlterado={valor => setImagem(valor)}
+                />
+                <ListaSuspensa 
+                    label="Gênero"
+                    itens={generos}
+                    obrigatorio={true}
+                    valor={genero}
+                    aoAlterado={valor => setGenero(valor)}
+                />
                 <Botao>
                     Criar Card
                 </Botao>
